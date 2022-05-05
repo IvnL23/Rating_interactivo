b81a8fe12eb735d847abd24e9cc139803b7c51fe
IvnL23 <ivanleandrofrias@gmail.com>
2022-05-05 02:10:08 -0400

    v0.1.0

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 var contenedorPrincipal = document.querySelector(".principal");
 var contenedorEnviar = document.querySelector(".contenedor-enviar");
-var botonEnviar = document.querySelector(".boton-enviar");
-var botonesEstrellas = document.getElementsByClassName("boton-satisfaccion");
+var botonEnviar = document.querySelector(".btnSubmit");
+const btnScore = document.getElementsByClassName("btnScore");
 var valorRating = document.querySelector("#estrella-elegida");
 
+document.getElementById("botn1").classList.remove("activo");
+document.getElementById("botn2").classList.remove("activo");
+document.getElementById("botn3").classList.remove("activo");
+document.getElementById("botn4").classList.remove("activo");
+document.getElementById("botn5").classList.remove("activo");
 
 var valorSeleccionado = "";
 
-for (let i = 0; i < botonesEstrellas.length; i++) {
-  botonesEstrellas[i].addEventListener("click", function () {
-    valorSeleccionado = botonesEstrellas[i].innerHTML;
+for (let i = 0; i < btnScore.length; i++) {
+  btnScore[i].addEventListener("click", function () {
+    document.getElementById("botn1").classList.remove("activo");
+    document.getElementById("botn2").classList.remove("activo");
+    document.getElementById("botn3").classList.remove("activo");
+    document.getElementById("botn4").classList.remove("activo");
+    document.getElementById("botn5").classList.remove("activo");
+    valorSeleccionado = btnScore[i].innerHTML;
+    btnScore[i].classList.add("activo");
   });
 }
 
@@ -19,6 +30,6 @@ botonEnviar.addEventListener("click", function () {
     contenedorEnviar.style.display = "flex";
     valorRating.innerHTML = valorSeleccionado;
   } else {
-    alert("Debe seleccionar un valor.");
+    alert("Please, rate us!");
   }
 });
